07a8dedc76c7f81944f52b2dc857a045a114c21f
Marina Chaccur <hi@marinachaccur.design>
2021-06-23 14:30:03 +0200

    Update makeImages.py
    Rotation elements

--- makeImages.py
+++ makeImages.py
@@ -1,11 +1,11 @@
-cellSize = 50
+cellSize = 250
 images = {}
-im = ImageObject('custom-images/leaf.png')
-im = ImageObject()
-with im:
-    size(cellSize, cellSize)
-    font('myleaves curvepoint', cellSize*2.5)
-    text('b', (0, 0))
+im = ImageObject('custom-images/Amz1.png')
+# im = ImageObject()
+# with im:
+#     size(cellSize, cellSize)
+#     font('myleaves curvepoint', cellSize*2.5)
+#     text('b', (0, 0))
 images['b'] = im
 
 # derive d, p, q from b
@@ -29,6 +29,43 @@ with q:
     scale(-1, -1)
     image(im, (0, 0))
 images['q'] = q
+
+a = ImageObject()
+with a:
+    translate(im.size()[0], 0)
+    rotate (90)
+    image(im, (0, 0))
+images['a'] = a
+
+c = ImageObject()
+with c:
+    translate(0, im.size()[1])
+    rotate (-90)
+    image(im, (0, 0))
+images['c'] = c
+
+e = ImageObject()
+with e:
+    # translate(im.size()[0], 0)
+    rotate (-90, (cellSize/2, cellSize/2))
+    image(d, (0, 0))
+images['e'] = e
+
+f = ImageObject()
+with f:
+    # translate(0, im.size()[1])
+    rotate (90, (cellSize/2, cellSize/2))
+    image(d, (0, 0))
+images['f'] = f
+
+# '1' = b
+# '2' = a
+# '3' = q
+# '4' = c
+# '5' = e
+# '6' = d
+# '7' = f
+# '8' = p
 
 for char in images:
     newDrawing()
